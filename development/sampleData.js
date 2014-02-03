6b613a1b3408a7296194e978a347c0e098f702af
nathanallen <thenathanator@gmail.com>
2014-02-03 10:21:04 -0800

    development directory for sample data and secrets

---
+++ development/sampleData.js
@@ -0,0 +1,29 @@
+var SampleData = {
+  movieData: [
+    {title: "Frozen", year: 2013, audience_rating: 46, critics_rating: 62},
+    {title: "Big Trouble in Little China", year: 1986, audience_rating: 78, critics_rating: 83},
+    {title: "Mad Max", year: 1979, audience_rating: 71, critics_rating: 95},
+    {title: "Shogun Assasin", year: 1980},
+    {title: "Wizards", year: 1977, audience_rating: 72, critics_rating: 59},
+    {title: "Brainscan", year: 1994, audience_rating: 62, critics_rating: 20},
+    {title: "Evita", year: 1996, audience_rating: 69, critics_rating: 64},
+    {title: "They Shall Have Music", year: 1938, audience_rating: 75, critics_rating: -1},
+    {title: "Fist of the North Star", year: 1986, audience_rating: 28, critics_rating: -1},
+    {title: "Underworld: Awakening", year: 2012, audience_rating: 62, critics_rating: 26},
+    {title: "The Vow", year: 2012, audience_rating: 63, critics_rating: 29},
+    {title: "Love Potion No. 9", year: 1992},
+    {title: "The Phantom Carriage", year: 1921, audience_rating: 89, critics_rating: 100},
+    {title: "After.Life", year: 2009, audience_rating: 67, critics_rating: -1},
+    {title: "Siddhartha", year: 1972},
+    {title: "Antarctica", year: 1983, audience_rating: 35, critics_rating: 50},
+    {title: "The Miracle", year: 1991, audience_rating: 36, critics_rating: 63},
+    {title: "Boyz n the Hood", year: 1991, audience_rating: 93, critics_rating: 96},
+    {title: "Behind the Blue", year: 1995, audience_rating: 0, critics_rating: -1},
+    {title: "The Mosquito Coast", year: 1986, audience_rating: 62, critics_rating: 75},
+    {title: "Boss Nigger", year: 1975, audience_rating: 72, critics_rating: -1},
+    {title: "Stay Tuned", year: 1992, audience_rating: 55, critics_rating: 46},
+    {title: "Robot Carnival", year: 1987, audience_rating: 78, critics_rating: -1},
+    {title: "W.W. and the Dixie Dancekings", year: 1975, audience_rating: 38, critics_rating: -1},
+    {title: "Don's Party", year: 1976, audience_rating: 67, critics_rating: -1}
+  ]
+}
