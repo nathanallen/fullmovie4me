28cad2c333586cb4980cae20f9b3d1d362aefa57
nathanallen <thenathanator@gmail.com>
2014-09-09 22:25:09 -0700

    move all movie processing to backend w/ api endpoint

--- fullmovie4me/fullmovie4me.py
+++ fullmovie4me/fullmovie4me.py
@@ -1,33 +1,39 @@
 from google.appengine.api import users
 from google.appengine.ext import ndb
-import webapp2, json, datetime
+import movie
+import webapp2, json
 
-
-class Movie(ndb.Model):
-  title = ndb.StringProperty()
-  year = ndb.IntegerProperty()
-  audience_rating = ndb.IntegerProperty()
-  critics_rating = ndb.IntegerProperty()
-  youtube_url = ndb.StringProperty()
-  creation_ts = ndb.DateTimeProperty(required=True, auto_now_add=True)
-  # title, year, audience_rating, critics_rating, youtube_url
-
-class MainPage(webapp2.RequestHandler):
+class ApiHandler(webapp2.RequestHandler):
   def get(self):
-    movies = Movie.query().order(-Movie.creation_ts).fetch(20)
-    movies = [movie.to_dict() for movie in movies]
-    print movies
+    print "HERE"
+    movies = movie.Movie.query().order(-movie.Movie.creation_ts).fetch(1000)
+    movies = [amovie.to_dict(exclude=['creation_ts']) for amovie in movies]
+    movies = dict([(amovie.get('title',''), amovie) for amovie in movies]).values() # dirty filter for uniques
+    movie_list = json.dumps(movies)
+    self.response.headers.add_header("Access-Control-Allow-Origin", "*")
+    self.response.headers['Content-Type'] = 'application/javascript'
+    self.response.out.write(movie_list)
 
   @ndb.toplevel #This tells the handler not to exit until its asynchronous requests have finished
   def post(self):
     movies_str = self.request.params.get('movies')
+    print movies_str
     movies = json.loads(movies_str)
-    ndb.put_multi_async([Movie(**movie) for movie in movies])
+    ndb.put_multi_async([movie.Movie(**amovie) for amovie in movies])
     self.response.headers.add_header("Access-Control-Allow-Origin", "*")
     self.response.headers['Content-Type'] = '*.*'
     self.response.write('Hello')
+
+class ViewHandler(webapp2.RequestHandler):
+  def get(self):
+    print "HERE"
+    self.response.write('Hello')
     
+  def post(self):
+    print "HERE"
+    self.response.write('Hello')
 
 application = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/', ViewHandler),
+    ('/api', ApiHandler),
 ], debug=True)
---
+++ fullmovie4me/movie.py
@@ -0,0 +1,95 @@
+from google.appengine.ext import ndb
+import urllib2, json, re, time
+
+class Movie(ndb.Model):
+  title = ndb.StringProperty()
+  year = ndb.IntegerProperty()
+  audience_rating = ndb.IntegerProperty()
+  critics_rating = ndb.IntegerProperty()
+  youtube_url = ndb.StringProperty() # any video url
+  creation_ts = ndb.DateTimeProperty(required=True, auto_now_add=True)
+  # title, year, audience_rating, critics_rating, youtube_url
+
+def parse_title_and_year(post_title):
+  if post_title == None or post_title == '':
+    return None, None
+  match = re.search(r'([^\(]*?) \((\d{4})\).?', post_title)
+  # TODO: capture "Season 1 / Episode 2" info
+  if not match:
+    return None, None
+  try:
+    movie_title, movie_year = match.group(1, 2)
+  except:
+    return None, None
+  return movie_title, int(movie_year)
+
+def fetch_movie_listings():
+  data = urllib2.urlopen('http://reddit.com/r/fullmoviesonanything.json').read()
+  listings = json.loads(data)['data']['children']
+  for listing in listings:
+    url = listing['data']['url']
+    # if Movie.query(Movie.youtube_url == url):
+    #   continue
+    post_title = listing['data']['title']
+    title, year = parse_title_and_year(post_title)
+    yield title, year, url
+
+def build_rotten_api_request_url(title):
+  ROTTEN_KEY = "7ru5dxvkwrfj8yfx36ymhch7"
+  BASE_URL = "http://api.rottentomatoes.com/api/public/v1.0"
+  request_url = BASE_URL
+  request_url += "/movies.json?"
+  request_url += "q=" + urllib2.quote(title)
+  request_url += "&page_limit=5&page=1"
+  request_url += "&apikey=" + ROTTEN_KEY
+  return request_url
+
+def query_rotten_api(title):
+  if title == None:
+    return None
+  request_url = build_rotten_api_request_url(title)
+  request_data = urllib2.urlopen(request_url).read()
+  return json.loads(request_data)
+
+def fetch_movie_data(title, year):
+  if not title:
+    return None
+  data = query_rotten_api(title)
+  if not data:
+    return None
+  movies = data.get('movies', None)
+  if not movies:
+    return None
+  match = None
+  for movie in movies:
+    if movie.get('year') != year:
+      continue
+    # TODO: fuzzy matching on year
+    # TODO: double check title is correct
+    match = movie
+    break
+  if match == None:
+    print "no match on year: guessing"
+    match = movies[0]
+  return match
+
+def fetch_new_movies_and_ratings(overwrite=False):
+  for title, year, url in fetch_movie_listings():
+    if not title:
+      continue
+    exists = Movie.query(Movie.youtube_url == url).get() # TODO: keys only req?
+    if exists:
+      if overwrite:
+        exists.key.delete()
+      continue
+    time.sleep(5)
+    match = fetch_movie_data(title, year)
+    if not match:
+      continue
+    ratings = match.get('ratings', {})
+    Movie.put(Movie(**{ "title": match.get('title') or title, 
+                        "year": year,
+                        "url": url,
+                        "audience_rating": ratings.get('audience_score'),
+                        "critics_rating": ratings.get('critics_score')
+                      }))
